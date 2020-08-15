80c9903cb518a8bb51e3a88f7f25f92b1efde801
Shuvojit Kar <shuvojitkar241@gmail.com>
2020-08-16 02:43:30 +0600

    added wallet screen and features screen

--- lib/src/view/shopper/wallet.dart
+++ lib/src/view/shopper/wallet.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:slashit/src/resources/text_styles.dart';
 
 class WalletScreen extends StatefulWidget {
   static const routeName = "/wallet";
@@ -12,50 +12,60 @@ class _WalletScreenState extends State<WalletScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Align(
-            alignment: Alignment.topLeft,
-            child: Container(
-              margin: EdgeInsets.only(top: 50, left: 20),
-              child: Icon(
-                FontAwesomeIcons.backward,
-                size: 30,
+      appBar: _header(),
+      body: Center(
+        child: Container(
+          height: 350,
+          width: 320,
+          child: Column(
+            children: <Widget>[
+              Text(
+                  "Pay Instantly from your Wallet balance using this scancode"),
+              SizedBox(
+                height: 30,
+              ),
+              QrImage(
+                constrainErrorBounds: true,
+                data: "1234567890",
+                version: QrVersions.auto,
+                size: 250.0,
+                gapless: false,
+              ),
+              Text("Capture QrCode instead")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _header() {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(100),
+      child: Container(
+        height: 100,
+        width: double.infinity,
+        child: Stack(
+          children: <Widget>[
+            GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: Padding(
+                padding: EdgeInsets.only(left: 20, top: 50),
+                child: Text("Cancel", style: debitCards),
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.topRight,
-            child: Container(
-              margin: EdgeInsets.only(top: 50, right: 20),
-              child: Text("Add Money"),
-            ),
-          ),
-          Center(
-            child: Container(
-              height: 350,
-              width: 320,
-              child: Column(
-                children: <Widget>[
-                  Text(
-                      "Pay Instantly from your Wallet balance using this scancode"),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  QrImage(
-                    constrainErrorBounds: true,
-                    data: "1234567890",
-                    version: QrVersions.auto,
-                    size: 250.0,
-                    gapless: false,
-                  ),
-                  Text("Capture QrCode instead")
-                ],
+            GestureDetector(
+              onTap: () {},
+              child: Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: EdgeInsets.only(right: 20, top: 50),
+                  child: Text("Add Money", style: debitCards),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
